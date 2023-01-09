5584daff45d3a1c5b1e56e43f15f967bb0acf275
luyao.chen <luyaochen@Lus-MacBook-Pro.local>
2023-01-08 17:51:03 -0800

    adding endpoint for do

--- receive_msg.py
+++ receive_msg.py
@@ -8,6 +8,10 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
 
+@app.route("/")
+def hi():
+    return '<h1> this is testing! </h1>'
+
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
     # info from sender
